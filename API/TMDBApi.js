2a1425a5859a1c4182cc5cf1a77c7af70b3426ea
JeremieGKlee <jeremie.gosgnack@gmail.com>
2020-07-14 15:11:59 +0200

    (test)

---
+++ API/TMDBApi.js
@@ -0,0 +1,14 @@
+
+
+const API_TOKEN = "d0c3ec8dffc59c765f236e4cc01aa446";
+
+export function getFilmsFromApiWithSearchedText (text) {
+    const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text
+    return fetch(url)
+      .then((response) => response.json())
+      .catch((error) => console.error(error))
+}
+
+export function getImageFromApi (name) {
+    return 'https://image.tmdb.org/t/p/w300' + name
+}
