9d7830ede103969925910f8213229ea3f8bc7280
JeremieGKlee <jeremie.gosgnack@gmail.com>
2020-07-27 15:56:24 +0200

    4

--- Components/FilmDetail.js
+++ Components/FilmDetail.js
@@ -1,19 +1,46 @@
 import React from 'react'
-import { StyleSheet, View, Text, ActivityIndicator, ScrollView, Image, TouchableOpacity } from 'react-native'
+import { StyleSheet, View, Text, ActivityIndicator, ScrollView, Image, TouchableOpacity, Share, Platform } from 'react-native'
 import { getFilmDetailFromApi, getImageFromApi } from '../API/TMDBApi'
 import moment from'moment'
 import numeral from 'numeral'
 import { connect } from 'react-redux'
+import EnlargeShrink from '../Animations/EnlargeShrink'
 
 
 class FilmDetail extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            film: undefined, // Pour l'instant on n'a pas les infos du film, on initialise donc le film à undefined.
-            isLoading: true // A l'ouverture de la vue, on affiche le chargement, le temps de récupérer le détail du film
-        }
+
+  static navigationOptions = ({ navigation }) => {
+    const { params } = navigation.state
+    if (params.film != undefined && Platform.OS === 'ios') {
+      return {
+          headerRight: <TouchableOpacity
+                          style={styles.share_touchable_headerrightbutton}
+                          onPress={() => params.shareFilm()}>
+                          <Image
+                            style={styles.share_image}
+                            source={require('../Images/ic_share.png')} />
+                        </TouchableOpacity>
+      }
     }
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      film: undefined, // Pour l'instant on n'a pas les infos du film, on initialise donc le film à undefined.
+      isLoading: true // A l'ouverture de la vue, on affiche le chargement, le temps de récupérer le détail du film
+    }
+
+    this._toggleFavorite = this._toggleFavorite.bind(this)
+    this._shareFilm = this._shareFilm.bind(this)
+  }
+
+  _updateNavigationParams() {
+    this.props.navigation.setParams({
+      shareFilm: this._shareFilm,
+      film: this.state.film
+    })
+  }
 
     // componentDidMount() {
     //     // console.log("Component FilmDetail monté")
@@ -28,7 +55,8 @@ class FilmDetail extends React.Component {
     // }
 
     componentDidMount() {
-      const favoriteFilmIndex = this.props.favoritesFilm.findIndex(item => item.id === this.props.route.params.idFilm)
+      const favoriteFilmIndex = this.props.favoritesFilm.findIndex(item =>
+         item.id === this.props.route.params.idFilm)
       if (favoriteFilmIndex !== -1) { // Film déjà dans nos favoris, on a déjà son détail
         // Pas besoin d'appeler l'API ici, on ajoute le détail stocké dans notre state global au state de notre component
         this.setState({
@@ -43,7 +71,7 @@ class FilmDetail extends React.Component {
         this.setState({
           film: data,
           isLoading: false
-        })
+        }, () => {this._updateNavigationParams() })
       })
     }
 
@@ -69,52 +97,80 @@ class FilmDetail extends React.Component {
         this.props.dispatch(action)
     }
 
-    _displayFilm() {
-        const { film } = this.state
-        if (film != undefined) {
-          return (
-            <ScrollView style={styles.scrollview_container}>
-              <Image
-                style={styles.image}
-                source={{uri: getImageFromApi(film.backdrop_path)}}
-              />
-              <Text style={styles.title_text}>{film.title}</Text>
-              <TouchableOpacity
-                style={styles.favorite_container}
-                onPress={() => this._toggleFavorite()}>
-                {this._displayFavoriteImage()}
-              </TouchableOpacity>
-              <Text style={styles.description_text}>{film.overview}</Text>
-              <Text style={styles.default_text}>Sorti le {moment(new Date(film.release_date)).format('DD/MM/YYYY')}</Text>
-              <Text style={styles.default_text}>Note : {film.vote_average} / 10</Text>
-              <Text style={styles.default_text}>Nombre de votes : {film.vote_count}</Text>
-              <Text style={styles.default_text}>Budget : {numeral(film.budget).format('0,0[.]00 $')}</Text>
-              <Text style={styles.default_text}>Genre(s) : {film.genres.map(function(genre){
-                  return genre.name;
-                }).join(" / ")}
-              </Text>
-              <Text style={styles.default_text}>Companie(s) : {film.production_companies.map(function(company){
-                  return company.name;
-                }).join(" / ")}
-              </Text>
-            </ScrollView>
-          )
-        }
-      }
-
     _displayFavoriteImage() {
         var sourceImage = require('../Images/ic_favorite_border.png')
+        var shouldEnlarge = false // Par default, si le film n'est pas en favori, on veut qu'au
+        //clic sur le bouton, celui-ci s'agrandisse=> shouldEnlarge à true
         if (this.props.favoritesFilm.findIndex(item => item.id ===
             this.state.film.id) !== -1) {
                 //film déjà dans les favoris
                 sourceImage = require('../Images/ic_favorite.png')
+                shouldEnlarge = true // Si le film est dans les favoris, on veut qu'au clic sur
+                // le bouton, celui-ci se rétrécisse => shouldEnlarge à false
             }
             return (
+              <EnlargeShrink
+                shouldEnlarge={shouldEnlarge}>
                 <Image
                     style={styles.favorite_image}
                     source={sourceImage}
                 />
+              </EnlargeShrink>
             )
+    }
+
+    _displayFilm() {
+      const { film } = this.state
+      if (film != undefined) {
+        return (
+          <ScrollView style={styles.scrollview_container}>
+            <Image
+              style={styles.image}
+              source={{uri: getImageFromApi(film.backdrop_path)}}
+            />
+            <Text style={styles.title_text}>{film.title}</Text>
+            <TouchableOpacity
+              style={styles.favorite_container}
+              onPress={() => this._toggleFavorite()}>
+              {this._displayFavoriteImage()}
+            </TouchableOpacity>
+            <Text style={styles.description_text}>{film.overview}</Text>
+            <Text style={styles.default_text}>Sorti le {moment(new Date(film.release_date)).format('DD/MM/YYYY')}</Text>
+            <Text style={styles.default_text}>Note : {film.vote_average} / 10</Text>
+            <Text style={styles.default_text}>Nombre de votes : {film.vote_count}</Text>
+            <Text style={styles.default_text}>Budget : {numeral(film.budget).format('0,0[.]00 $')}</Text>
+            <Text style={styles.default_text}>Genre(s) : {film.genres.map(function(genre){
+                return genre.name;
+              }).join(" / ")}
+            </Text>
+            <Text style={styles.default_text}>Companie(s) : {film.production_companies.map(function(company){
+                return company.name;
+              }).join(" / ")}
+            </Text>
+          </ScrollView>
+        )
+      }
+    }
+
+    _shareFilm() {
+      const { film } = this.state
+      Share.share({ title: film.title, message: film.overview })
+    }
+
+    _displayFloatingActionButton() {
+      const { film } = this.state
+      if (film != undefined && Platform.OS === 'android') {
+        //uniquement sur android et lorsque le film est chargé
+        return (
+          <TouchableOpacity
+            style={styles.share_touchable_floatingactionbutton}
+            onPress={() => this._shareFilm()}>
+            <Image
+              style={styles.share_image}
+              source={require('../Images/ic_share.png')} />
+            </TouchableOpacity>
+        )
+      }
     }
 
     render() {
@@ -126,6 +182,7 @@ class FilmDetail extends React.Component {
                 {this._displayLoading()}
                 {this._displayFilm()}
                 {/* <Text>Détail du film {this.props.route.params.idFilm}</Text> */}
+                {this._displayFloatingActionButton()}
             </View>
         )
     }
@@ -179,9 +236,28 @@ const styles = StyleSheet.create({
     },
 
     favorite_image: {
-      width: 40,
-      height: 40
+      flex: 1,
+      width: null,
+      height: null
 
+    },
+    share_touchable_floatingactionbutton: {
+      position: 'absolute',
+      width: 60,
+      height: 60,
+      right: 30,
+      bottom: 30,
+      borderRadius: 30,
+      backgroundColor: '#e91e63',
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    share_touchable_headerrightbutton: {
+      marginRight: 8
+    },
+    share_image: {
+      width: 30,
+      height: 30
     }
   })
 
--- Components/test.js
+++ Components/test.js
@@ -1,32 +1,127 @@
-// Components/Test.js
+
 
 import React from 'react'
-import { StyleSheet, View, Platform, Text } from 'react-native'
-
+import { StyleSheet, View, Platform, Text, Animated, Easing, PanResponder, Dimensions} from 'react-native'
+import HelloWorld from './HelloWorld'
 
 class Test extends React.Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      topPosition: 0,
+      leftPosition: 0,
+    }
+
+  var {height, width} = Dimensions.get('window');
+  this.panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: (evt, gestureState) => true,
+    onPanResponderMove: (evt, gestureState) => {
+      let touches = evt.nativeEvent.touches;
+      if (touches.length == 1) {
+        this.setState({
+          topPosition: touches[0].pageY - height/2,
+          leftPosition: touches[0].pageX - width/2
+        })
+      }
+    }
+  })
+}
+  
   render() {
     return (
-      
       <View style={styles.main_container}>
-        { Platform.OS === 'ios' ? <Text>iOS</Text> : <Text>Android</Text> }
-        <View style={styles.subview_container}>
+        <HelloWorld/>
+        <View
+          {...this.panResponder.panHandlers}
+          style={[styles.animation_view, { top: this.state.topPosition, left:
+          this.state.leftPosition }]}>
         </View>
       </View>
     )
   }
+
+
+
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     topPosition: new Animated.Value(0),
+  //     leftPosition: new Animated.Value(0),
+  //   }
+  // }
+
+  // componentDidMount() {
+  //   Animated.parallel([
+  //     Animated.spring(
+  //       this.state.topPosition,
+  //       {
+  //         toValue: 100,
+  //         tension: 8,
+  //         friction: 3
+  //       }
+  //     ),
+  //     Animated.timing(
+  //       this.state.leftPosition,
+  //       {
+  //         toValue: 100,
+  //         duration: 1000,
+  //         easing: Easing.elastic(2)
+  //       }
+  //     )
+  //   ]).start()
+  // }
+
+  // render() {
+  //   return (
+      
+  //     <View style={styles.main_container}>
+  //       {/* { Platform.OS === 'ios' ? <Text>iOS</Text> : <Text>Android</Text> } */}
+  //       <HelloWorld/>
+  //       {/* <View style={styles.subview_container}> */}
+  //       {/* <View style={styles.animation_view}>
+  //       </View> */}
+  //       <Animated.View style={[styles.animation_view, { top:
+  //       this.state.topPosition, left: this.state.leftPosition }]}>
+  //       </Animated.View>
+
+  //     </View>
+  //   )
+  // }
+
+
+  // componentDidMount() {
+  //   Animated.sequence([
+  //     Animated.spring(
+  //       this.state.topPosition,
+  //       {
+  //         toValue: 100,
+  //         tension: 8,
+  //         friction: 3
+  //       }
+  //     ),
+  //     Animated.timing(
+  //       this.state.topPosition,
+  //       {
+  //         toValue: 0,
+  //         duration: 1000,
+  //         easing: Easing.elastic(2)
+  //       }
+  //     )
+  //   ]).start()
+  // }
 }
-
-
-
 const styles = StyleSheet.create({
   main_container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
   },
-
+  animation_view: {
+      backgroundColor: 'red',
+      width: 100,
+      height: 100
+  }
   // Soit on utilise la fonction Platform.select
   // subview_container: {
   //   ...Platform.select({
@@ -36,20 +131,23 @@ const styles = StyleSheet.create({
   //       width: 50
   //     },
   //     android: {
-  //       backgroundColor: 'blue',
+  //       backgroundColor: 'red',
   //       height: 50,
   //       width: 100
   //     }
   //   })
   // },
 
+  
+
+
 // Soit on teste la valeur de l'OS
 
-subview_container: {
-    backgroundColor: Platform.OS === 'ios' ? 'red' : 'blue',
-    height: Platform.OS === 'ios' ? 100 : 50,
-    width: Platform.OS === 'ios' ? 50 : 100
-}
+// subview_container: {
+//     backgroundColor: Platform.OS === 'ios' ? 'red' : 'blue',
+//     height: Platform.OS === 'ios' ? 100 : 50,
+//     width: Platform.OS === 'ios' ? 50 : 100
+// }
 
 })  
 export default Test
